c79618e5adc9900acf201a9721a65b7e51ff79a1
Prajwal-PL <prajwalpl096@gmail.com>
2024-04-13 23:18:05 +0530

    Added multiple UI components from Aceternity UI and shadcn UI in the HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,51 @@
+import { ContainerScroll } from "@/components/global/ContainerScroll";
+import { HeroParallax } from "@/components/global/hero-parallax";
+import { InfiniteMovingCards } from "@/components/global/inf-moving-cards";
+import { LampComponent } from "@/components/global/lamp-component";
 import Navbar from "@/components/global/Navbar";
+import { Button } from "@/components/ui/button";
+import { clients, products } from "@/lib/constant";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative flex flex-col items-center antialiased">
+        <div className="max-w-4xl mx-auto p-4 relative z-10 flex items-center justify-center flex-col"></div>
+        <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+          <ContainerScroll
+            titleComponent={
+              <div className="flex items-center justify-center flex-col">
+                <Button
+                  size={"lg"}
+                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+                >
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black group-hover:to-black">
+                    Start For Free Today
+                  </span>
+                </Button>
+                <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                  Automate Your Work With Pulse
+                </h1>
+              </div>
+            }
+          />
+        </div>
+      </section>
+      <InfiniteMovingCards
+        className="md:mt-[18rem] mt-[-100px]"
+        items={clients}
+        direction="right"
+        speed="slow"
+      />
+      <section>
+        <HeroParallax products={products}></HeroParallax>
+      </section>
+      <section className="mt-[-500px]">
+        <LampComponent />
+      </section>
     </main>
   );
 }
