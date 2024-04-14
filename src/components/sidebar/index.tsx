371cc40df66c8e1dfc71d94e13edb9a6e8ea9f9d
Prajwal-PL <prajwalpl096@gmail.com>
2024-04-14 15:07:21 +0530

    completed sidebar component

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import {
   Tooltip,
@@ -8,16 +9,21 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { menuOptions } from "@/lib/constant";
-import { usePathname } from "next/navigation";
 import clsx from "clsx";
+import { Separator } from "@/components/ui/separator";
+import { Database, GitBranch, LucideMousePointerClick } from "lucide-react";
+import { ModeToggle } from "../global/mode-toggle";
 
-const MenuOptions = () => {
+type Props = {};
+
+const MenuOptions = (props: Props) => {
   const pathName = usePathname();
+
   return (
     <nav className=" dark:bg-black h-screen overflow-scroll  justify-between flex items-center flex-col  gap-10 py-6 px-2">
-      <div className="flex items-center flex-col gap-8 justify-center">
-        <Link href="/" className="flex font-bold flex-row">
-          Pulse
+      <div className="flex items-center justify-center flex-col gap-8">
+        <Link className="flex font-bold flex-row " href="/">
+          pulse.
         </Link>
         <TooltipProvider>
           {menuOptions.map((menuItem) => (
@@ -41,10 +47,37 @@ const MenuOptions = () => {
                     </Link>
                   </li>
                 </TooltipTrigger>
+                <TooltipContent
+                  side="right"
+                  className="bg-black/10 backdrop-blur-xl"
+                >
+                  <p>{menuItem.name}</p>
+                </TooltipContent>
               </Tooltip>
             </ul>
           ))}
         </TooltipProvider>
+        <Separator />
+        <div className="flex items-center flex-col gap-9 dark:bg-[#353346]/30 py-4 px-2 rounded-full h-56 overflow-scroll border-[1px]">
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <LucideMousePointerClick className="dark:text-white" size={18} />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]" />
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <GitBranch className="text-muted-foreground" size={18} />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]"></div>
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <Database className="text-muted-foreground" size={18} />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]"></div>
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <GitBranch className="text-muted-foreground" size={18} />
+          </div>
+        </div>
+      </div>
+      <div className="flex bottom-0 justify-end flex-col gap-8">
+        <ModeToggle />
       </div>
     </nav>
   );
